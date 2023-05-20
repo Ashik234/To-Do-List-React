7287b178b00810185371ac30f23c41ec0314f316
Ashik k <ashikash796@gmail.com>
2023-05-20 17:55:36 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import './App.css';
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+
+function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+
+  const currentDayOfWeek = new Date().toLocaleString(
+    "default",
+    { weekday: "long" }
+  );
+
+  const handleAddToDo = () => {
+    if (toDo.trim() === '') {
+      Swal.fire({
+        icon: 'error',
+        text: 'Please enter a valid item!',
+      });
+      return;
+    }
+
+    const isDuplicate = toDos.some((item) => item.text === toDo);
+
+    if (isDuplicate) {
+      Swal.fire({
+        icon: 'error',
+        text: 'This item already exists in the list!',
+      });
+    } else {
+      setToDos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+      setToDo('');
+    }
+  };
+  
+  const handleDelete = (id) => {
+    setToDos(toDos.filter((obj) => obj.id !== id));
+  };
+
+  return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoops, it's {currentDayOfWeek} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={handleAddToDo} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {
+          toDos.map((obj) => {
+          return (
+            <div className="todo" key={obj.id}>
+              <div className="left">
+                <input onChange={(e) => {
+                     setToDos(toDos.map((obj2) => {
+                        if (obj2.id === obj.id) {
+                           obj2.status = e.target.checked
+                        }
+                        return obj2;
+                      })
+                    );
+                  }} value={obj.status} type="checkbox" name="" id=""/>
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i onClick={() => handleDelete(obj.id)}className="fas fa-times"></i>
+              </div>
+            </div>
+          );
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
